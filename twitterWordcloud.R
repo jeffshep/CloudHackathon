c27df393be6139cf8a57c48a17a7a2cc63e0ef7f
Jeff Shepherd <jeffshep13@gmil.com>
2016-02-17 15:18:21 +0000

    Fixed the wordcloud function

--- twitterWordcloud.R
+++ twitterWordcloud.R
@@ -1,52 +1,21 @@
-#This work was developed over 2 days during a Cloud Hackathon. I was completely new to Amazon Web Services, so some time
-#was taken up configuring Instances and VMs. It was also the first time I had used an API for grab data.
-#This is a proof of concept to run data analysis in the browser. The code has some errors with respect to the 
-#TermDocumentMatrix() function - line 32. 
-#In general the code does the following:
-#Retrieves top 500 tweets for the user defined #hashtag.
-#Removes twitter handles, tidies the words up (remove punctuation & take the stem of common words)
-#Generates wordcloud from the frequency of words, saves as .png
+library(twitteR)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
 
-require(twitteR)
-rdmTweets <- searchTwitter('#exeter', n=500)
-tw.df <- do.call("rbind", lapply(rdmTweets, as.data.frame))
- 
-RemoveAtPeople <- function(tweet) {
-  gsub("@\\w+", "", tweet)
-}
+#setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
-tweets <- as.vector(sapply(tw.df$text, RemoveAtPeople))
- 
 
-require(tm)
-#Call with eg: tw.c=generateCorpus(tw.df$text)
-generateCorpus= function(df,my.stopwords=c()){
-  tw.corpus= Corpus(VectorSource(df))
-  tw.corpus = tm_map(tw.corpus, removePunctuation)
-  tw.corpus = tm_map(tw.corpus, tolower)
-  tw.corpus = tm_map(tw.corpus, removeWords, stopwords('english'))
-  tw.corpus = tm_map(tw.corpus, removeWords, my.stopwords)
-  tw.corpus = tm_map(tw.corpus, stemDocument)
- 
-  tw.corpus
-}
+hashtag <- searchTwitter("#weather", n=2500)
+hashtag_text <- sapply(hashtag, function(x) x$getText())
+hashtag_text_corpus <- Corpus(VectorSource(hashtag_text))
+hashtag_text_corpus <- tm_map(hashtag_text_corpus,
+                                     content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')),
+                                     mc.cores=1
+)
+hashtag_text_corpus <- tm_map(hashtag_text_corpus, content_transformer(tolower), mc.cores=1)
+hashtag_text_corpus <- tm_map(hashtag_text_corpus, removePunctuation, mc.cores=1)
+hashtag_corpus <- tm_map(hashtag_text_corpus, function(x)removeWords(x,stopwords()), mc.cores=1)
 
-wordcloud.generate=function(tw.corpus,min.freq=3){
-  require(wordcloud)
-  doc.m = TermDocumentMatrix(tw.corpus,control = list(minWordLength = 1))
-  dm = as.matrix(doc.m)
-  # calculate the frequency of words
-  v = sort(rowSums(dm), decreasing=TRUE)
-  d = data.frame(word=names(v), freq=v)
-  #Generate the wordcloud
-  wc=wordcloud(d$word, d$freq, min.freq=min.freq)
-  wc
-}
- 
-print(wordcloud.generate(generateCorpus(tweets,'dev8d'),7))
- 
-##Generate an image file of the wordcloud
-png(filename='~R/plots/twitter1')
-wordcloud.generate(generateCorpus(tweets,'dev8d'),7)
-dev.off()
- 
+pal2 <- brewer.pal(8,"Dark2")
+wordcloud(hashtag_text_corpus,min.freq=4,max.words=100, random.order=T, colors=pal2)
